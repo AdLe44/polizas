12c7e65ec0ffc76feb0fc533506d008cc263ef70
Kairos Manuel Adrian Millan Leal <nem.madrian@coppel.com>
2025-02-27 10:44:10 -0700

    pendiente

--- Database/4_Funciones/4.2_Inventario/4.2.2_fn_update_inventario.sql
+++ Database/4_Funciones/4.2_Inventario/4.2.2_fn_update_inventario.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION fn_insert_inventario (
+CREATE OR REPLACE FUNCTION fn_update_inventario (
     IN p_sku VARCHAR(10),
     IN p_nombre VARCHAR(100),
     IN p_cantidad INT
@@ -7,8 +7,10 @@ RETURNS VARCHAR(10) AS $$
 DECLARE
     v_sku VARCHAR(10);
 BEGIN
-    INSERT INTO Inventario (SKU, Nombre, Cantidad)
-    VALUES (p_sku, p_nombre, p_cantidad)
+    UPDATE Inventario
+    SET Nombre = p_nombre,
+        Cantidad = p_cantidad
+    WHERE SKU = p_sku
     RETURNING SKU INTO v_sku;
 
     RETURN v_sku;
